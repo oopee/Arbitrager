5c8f98c2fb2c7148489279f6d9d0982e44a90959
Petteri Laukkanen <lplaukkanen@gmail.com>
2018-01-04 07:46:17 +0200

    GDAXClient: Add source code instead of NuGet package

---
+++ 3rdParty/gdax-csharp/GDAXClient/Shared/ProductType.cs
@@ -0,0 +1,16 @@
+﻿namespace GDAXClient.Services.Orders
+{
+    public enum ProductType
+    {
+        BtcUsd,
+        BtcEur,
+        BtcGbp,
+        EthUsd,
+        EthEur,
+        EthBtc,
+        LtcUsd,
+        LtcEur,
+        LtcBtc,
+        BchUsd
+    }
+}
